ba681cd0fb4d42482ca4d8fc85d48c37bd4e0fff
Courses <courses@MacBook-Pro.local>
2022-07-19 16:50:03 +0100

    addingFonts

--- App.js
+++ App.js
@@ -1,39 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { useState, useEffect, useCallback } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import Total from "./Total";
 
 export default function App() {
-
-  const [numSandwiches, setNumSandwiches] = useState(0);
-  const [numWater, setNumWater] = useState(0);
-
-  const sandwichPrice = 5;
-
-  const addSandwich = () => {
-    setNumSandwiches(numSandwiches => numSandwiches + 1);
-  }
-
-  const addWater = () => {
-    setNumWater(numWater => numWater + 1);
-  }
-
-  const getTotal = useCallback(() => {
-    return numSandwiches * sandwichPrice;
-  }, [numSandwiches]);
 
   return (
     <SafeAreaProvider style={styles.container}>
       <SafeAreaView>
 
-        <Text style={styles.label}>{ "Sandwiches: " + numSandwiches }</Text>
-        <Button title="Add sandwich" onPress={addSandwich} />        
-
-        <Text style={styles.label}>{ "Water: " + numWater }</Text>
-        <Button title="Add water" onPress={addWater} />
-
-        <Total getTotal={getTotal} />
+        <Text style={styles.label}>Hi everyone!</Text>
 
       </SafeAreaView>
     </SafeAreaProvider>
