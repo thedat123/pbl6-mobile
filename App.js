0d48e980e6167462c626322f9bbee545cfe96048
Courses <courses@MacBook-Pro.local>
2022-07-19 16:25:06 +0100

    useCallback

--- App.js
+++ App.js
@@ -1,34 +1,40 @@
 import { StatusBar } from "expo-status-bar";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import Total from "./Total";
 
 export default function App() {
 
-  const [count, setCount] = useState(0);
-  const [newCount, setNewCount] = useState(0);
+  const [numSandwiches, setNumSandwiches] = useState(0);
+  const [numWater, setNumWater] = useState(0);
 
-  useEffect(() => {
-    console.log("I'm rendering");
-  }, [count, newCount])
+  const sandwichPrice = 5;
 
-  const add = () => {
-    setCount(prev => prev + 1);
-    setCount(prev => prev + 1);
+  const addSandwich = () => {
+    setNumSandwiches(numSandwiches => numSandwiches + 1);
   }
 
-  const minus = () => {
-    setNewCount(newCount - 1)
+  const addWater = () => {
+    setNumWater(numWater => numWater + 1);
   }
+
+  const getTotal = useCallback(() => {
+    return numSandwiches * sandwichPrice;
+  }, [numSandwiches]);
 
   return (
     <SafeAreaProvider style={styles.container}>
       <SafeAreaView>
 
-        <Button title="Add" onPress={add} />        
-        <Text style={styles.label}>{ count }</Text>
+        <Text style={styles.label}>{ "Sandwiches: " + numSandwiches }</Text>
+        <Button title="Add sandwich" onPress={addSandwich} />        
 
-        <Button title="Minus" onPress={minus} />
+        <Text style={styles.label}>{ "Water: " + numWater }</Text>
+        <Button title="Add water" onPress={addWater} />
+
+        <Total getTotal={getTotal} />
+
       </SafeAreaView>
     </SafeAreaProvider>
   );
