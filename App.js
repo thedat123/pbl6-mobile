f45d68080a80333a081d271fba872b861a97fe12
Courses <courses@MacBook-Pro.local>
2022-07-20 10:34:45 +0100

    icons

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import * as Font from 'expo-font';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import ChatListScreen from './screens/ChatListScreen';
 import ChatSettingsScreen from './screens/ChatSettingsScreen';
@@ -21,10 +22,16 @@ const TabNavigator = () => {
     <Tab.Navigator
       screenOptions={{ headerTitle: '' }}>
       <Tab.Screen name="ChatList" component={ChatListScreen} options={{
-        tabBarLabel: 'Chats'
+        tabBarLabel: 'Chats',
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="chatbubble-outline" size={ size } color={ color }  />
+        )
       }} />
       <Tab.Screen name="Settings" component={SettingsScreen} options={{
-        tabBarLabel: 'Settings'
+        tabBarLabel: 'Settings',
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="settings-outline" size={ size } color={ color } />
+        )
       }}/>
     </Tab.Navigator>
   )
