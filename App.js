f0cb040582e4545c1bfc2c0620238735f33f71a0
Courses <courses@MacBook-Pro.local>
2022-07-20 09:06:16 +0100

    navigationOptions

--- App.js
+++ App.js
@@ -66,7 +66,10 @@ export default function App() {
 
           <Stack.Navigator>
             <Stack.Screen name="Home" component={ChatListScreen} />
-            <Stack.Screen name="ChatSettings" component={ChatSettingsScreen} />
+            <Stack.Screen name="ChatSettings" component={ChatSettingsScreen} options={{
+              headerTitle: "Settings",
+              headerBackTitle: "Back"
+            }} />
           </Stack.Navigator>
 
         </NavigationContainer>
