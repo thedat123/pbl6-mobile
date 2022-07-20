ef3eda54e536916a0fb56abdbc7f82c699c2b9c0
Courses <courses@MacBook-Pro.local>
2022-07-20 13:42:53 +0100

    mainNavigator

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,54 +1,12 @@
 import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
 
-import ChatSettingsScreen from '../screens/ChatSettingsScreen';
-import SettingsScreen from '../screens/SettingsScreen';
-import ChatListScreen from "../screens/ChatListScreen";
-
-const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
-
-const TabNavigator = () => {
-  return (
-    <Tab.Navigator
-      screenOptions={{ headerTitle: '' }}>
-      <Tab.Screen name="ChatList" component={ChatListScreen} options={{
-        tabBarLabel: 'Chats',
-        tabBarIcon: ({ color, size }) => (
-          <Ionicons name="chatbubble-outline" size={ size } color={ color }  />
-        )
-      }} />
-      <Tab.Screen name="Settings" component={SettingsScreen} options={{
-        tabBarLabel: 'Settings',
-        tabBarIcon: ({ color, size }) => (
-          <Ionicons name="settings-outline" size={ size } color={ color } />
-        )
-      }}/>
-    </Tab.Navigator>
-  )
-}
+import MainNavigator from "./MainNavigator";
 
 const AppNavigator = (props) => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={TabNavigator}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="ChatSettings"
-          component={ChatSettingsScreen}
-          options={{
-            headerTitle: "Settings",
-            headerBackTitle: "Back",
-          }}
-        />
-      </Stack.Navigator>
+      <MainNavigator />
     </NavigationContainer>
   );
 };
